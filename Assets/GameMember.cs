641b7ea817f823471d8d717ffbd799ab26f80524
Florian Guyet <flo.guyet@gmail.com>
2016-11-04 10:39:42 -0300

    Player and Gameboard

---
+++ Assets/GameMember.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class GameMember : MonoBehaviour {
+    protected int pdv;
+    protected float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : GameMember {
+
+	// Use this for initialization
+	void Start () {
+        pdv = 5;
+        speed = 1; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Move up");
+            move(new Vector2(1,1));
+        }
+    }
+
+    void move(Vector2 direction)
+    {
+        Vector2 position = gameObject.transform.localPosition;
+        Vector2 newPosition = new Vector2(position.x + direction.x, position.y + direction.y);
+        gameObject.transform.localPosition = newPosition;
+    }
+}
